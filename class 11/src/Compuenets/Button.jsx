891c9efaea3060988923522e055be6928a4119d1
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-06-22 13:13:14 +0500

    commit

---
+++ class 11/src/Compuenets/Button.jsx
@@ -0,0 +1,13 @@
+
+import { Button } from '@mui/material'
+import React from 'react'
+
+const CustomBtn = ({title , variant}) => {
+  return (
+    <>
+    <Button variant={variant}>{title}</Button>
+    </>
+  )
+}
+
+export default CustomBtn

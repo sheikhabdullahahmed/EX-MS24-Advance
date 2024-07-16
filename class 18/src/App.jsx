6bf146db2b36513bb762bbe5a1117c4217fb0f77
Sheikh Abdullah Ahmed <abdullahsheikh456677@gmail.com>
2024-07-17 02:57:05 +0500

    class 18

--- class 18/src/App.jsx
+++ class 18/src/App.jsx
@@ -1,29 +1,91 @@
-import { useState } from "react";
+// // import { useState } from "react";
 
 
-export default function TextForm  () {
-  const handle = () => {
-    console.log("Upper Case Clicked");
-    let newText = text.toUpperCase()
-    setText(newText)
-  }
+// // export default function TextForm  () {
+// //   const handle = () => {
+// //     console.log("Upper Case Clicked");
+// //     let newText = text.toUpperCase()
+// //     setText(newText)
+// //   }
 
-  const handelonChange = (e) => {
-    console.log("On Change")
-    setText(e.target.value)
-
-
-  }
+// //   const handelonChange = (e) => {
+// //     console.log("On Change")
+// //     setText(e.target.value)
 
 
-  const [text, setText] = useState()
+// //   }
 
 
-  return (
-    <>
-    <h1>Text Form</h1>
-    <textarea name="" value={text} onChange={handelonChange} id=""></textarea>
-    <button onClick={handle}>Change</button>
-    </>
-  )
-}
+// //   const [text, setText] = useState()
+
+
+// //   return (
+// //     <>
+// //     <h1>Text Form</h1>
+// //     <textarea name="" value={text} onChange={handelonChange} id=""></textarea>
+// //     <button onClick={handle}>Change</button>
+// //     </>
+// //   )
+// // }
+
+
+
+// // import { useState } from "react";
+
+
+
+// // export default function TextFrom () {
+// //   const handle = () => {
+// //     console.log("UpperCase Click")
+// //     let newText = text.toUpperCase()
+// //     setText(newText)
+// //   }
+
+
+// import { useState } from "react";
+
+
+// export default function text () {
+//   const handle = () => {
+//     console.log("Uper Case")
+//     let newText = text.toUpperCase()
+//     setText(newText)
+ 
+//   }
+
+//   const handlefunc = (e) => {
+//     console.log("Click")
+//     setText(e.target.value)
+//   }
+
+//   const [text, setText ] = useState()
+
+//   return (
+//     <>
+//     <h1>Text</h1>
+//     <textarea name="" value={text} onChange={handlefunc} id=""></textarea>
+//     <button onClick={handle}>Click</button>
+//     </>
+//   )
+// }
+
+
+
+
+// //   const handleClick = (e) => {
+// //     console.log("Click")
+// //     setText(e.target.value)
+
+// //   }
+
+// //   const [text, setText] = useState()
+
+
+// //   return (
+// //     <>
+// //     <h1>Text Form</h1>
+// //     <textarea name="" value={text} onChange={handleClick} id=""></textarea>
+// //     <button onClick={handle}>Click</button>
+// //     </>
+// //   )
+// // }
